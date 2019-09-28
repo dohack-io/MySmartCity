2046cf19af6bb8b338e3ff37b0b6bbbf51afd071
Sven Treutler <sventreutler@gmail.com>
2019-09-28 13:01:12 +0200

    UI5 Save and Cancel functionality

--- frontend/webapp/controller/Home.controller.js
+++ frontend/webapp/controller/Home.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
+	"sap/m/MessageBox",
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/unified/FileUploader",
 	"../model/formatter"
-], function(Controller, FileUploader, formatter) {
+], function (MessageBox, Controller, FileUploader, formatter) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
@@ -11,15 +12,15 @@ sap.ui.define([
 
 		onInit: function () {
 			var oForm = this.getView().byId("FormContainer");
-			oForm.addFormElement(this.addInputField ("eins","Number","Platzhalter","Number"));
-			oForm.addFormElement(this.addInputField ("zwei","Text","Platzhalter","Text"));
-			oForm.addFormElement(this.addFileField ("drei","FileUpload","Platzhalter"));
-			oForm.addFormElement(this.addDateTimeInput ("vier","DateTime","Platzhalter","2018-12-17T03:24:00","2019-12-17T03:24:00"));
+			oForm.addFormElement(this.addInputField("eins", "Number", "Platzhalter", "Number"));
+			oForm.addFormElement(this.addInputField("zwei", "Text", "Platzhalter", "Text"));
+			oForm.addFormElement(this.addFileField("drei", "FileUpload", "Platzhalter"));
+			oForm.addFormElement(this.addDateTimeInput("vier", "DateTime", "Platzhalter", "2018-12-17T03:24:00", "2019-12-17T03:24:00"));
 
 
 		},
 
-		addInputField : function (isID, isLabel, isPlaceholder, isType) {
+		addInputField: function (isID, isLabel, isPlaceholder, isType) {
 			var newFormElement = this.addFormElement(isLabel);
 			var newField = new sap.m.Input(isID);
 			newField.setPlaceholder(isPlaceholder)
@@ -42,7 +43,7 @@ sap.ui.define([
 			return newFormElement;
 		},
 
-		addDateTimeInput: function (isID, isLabel, isPlaceholder,isMinDate,isMaxDate) {
+		addDateTimeInput: function (isID, isLabel, isPlaceholder, isMinDate, isMaxDate) {
 			var newFormElement = this.addFormElement(isLabel);
 			var newField = new sap.m.DateTimeInput(isID);
 			newField.setPlaceholder(isPlaceholder);
@@ -51,20 +52,67 @@ sap.ui.define([
 			return newFormElement;
 		},
 
-		OnSave: function () {
+		onSave: function () {
 			var oForm = this.getView().byId("FormContainer");
+			this.getValuesInForm(oForm);
+		},
 
+		getValuesInForm: function (oForm) {
 			var aElements = oForm.getFormElements();
-			aElements.forEach(function(oElement) {
+			aElements.forEach(function (oElement) {
 				console.log(oElement.getFields()[0].getId());
 				console.log(oElement.getFields()[0].getValue());
-			  });
-
+			});
 		},
 
 		onCancel: function () {
 			var oForm = this.getView().byId("FormContainer");
+			if (this.checkValuesInForm(oForm) == true) {
+				this.checkDeleteValues();
+				
+				
+			} else {
+				this.deleteValuesInForm(oForm);
+			}
+		},
 
+		checkDeleteValues: function () {
+			var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
+			var that = this;
+			MessageBox.error(
+				"Check! Delete?",
+				{
+					actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+					styleClass: bCompact ? "sapUiSizeCompact" : "",
+					onClose: function(sAction) {
+						if (sAction == MessageBox.Action.YES ){ 
+							var oForm = that.getView().byId("FormContainer");
+							that.deleteValuesInForm(oForm);
+						} else if (sAction == MessageBox.Action.NO ){ 
+						}
+					}
+				}
+			);
+
+		},
+
+		deleteValuesInForm: function (oForm) {
+			var aElements = oForm.getFormElements();
+			aElements.forEach(function (oElement) {
+				oElement.getFields()[0].setValue("");
+			});
+		},
+
+		checkValuesInForm: function (oForm) {
+			var rbCheckValues = false;
+			var aElements = oForm.getFormElements();
+			aElements.forEach(function (oElement) {
+				if (oElement.getFields()[0].getValue() !== "") {
+					rbCheckValues = true;
+
+				};
+			});
+			return rbCheckValues;
 		},
 
 
